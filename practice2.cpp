72a021ab89635b53054103d80aba53b428a38cfb
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-10-28 09:46:07 +0000

    update

---
+++ practice2.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+    // cout<<(10<<2)
+    // ;
+
+    int a=10;
+
+    switch(a){
+
+        case 1:
+        cout<<"the value is 1"<<endl;
+        break;
+
+        case 16:
+        cout<<"the value is 10"<<endl;
+        break;
+
+
+        default:
+        cout<<"no value in a"<<endl;
+    }
+    return 0;
+}
